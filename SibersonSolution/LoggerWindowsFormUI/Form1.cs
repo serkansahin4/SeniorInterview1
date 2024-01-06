a3c584863495f131af61098482ba0b29a91923d0
serkana607@gmail.com <serkana607@gmail.com>
2024-01-06 07:15:05 +0300

    was successful

---
+++ SibersonSolution/LoggerWindowsFormUI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoggerWindowsFormUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string logFilePath = Path.Combine(rootPath, "VeriketApp", "VeriketAppTest.txt");
+
+            DataTable dataTable = new DataTable();
+            using (StreamReader reader = new StreamReader(logFilePath))
+            {
+                DataColumn[] dataColumns= {
+                new DataColumn("Date"),
+                new DataColumn("ComputerName"),
+                new DataColumn("UserName")
+                };
+
+                dataTable.Columns.AddRange(dataColumns);
+                while (!reader.EndOfStream)
+                {
+                    string[] rows = reader.ReadLine().Split(',');
+                    dataTable.Rows.Add(rows);
+                }
+            }
+
+            dataGridView1.DataSource = dataTable;
+        }
+    }
+}
---
+++ SibersonSolution/LoggerWindowsService/Service1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace LoggerWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer.Interval = 60000;
+            timer.Elapsed += new ElapsedEventHandler(WriteLog);
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+
+        private void WriteLog(object sender, ElapsedEventArgs e)
+        {
+            string rootPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            string folderPath = Path.Combine(rootPath, "VeriketApp");
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            string logFilePath = Path.Combine(folderPath, "VeriketAppTest.txt");
+            if (!File.Exists(logFilePath))
+                File.Create(logFilePath);
+            
+            using (StreamWriter writer = File.AppendText(logFilePath))
+            {
+                writer.WriteLine(GetLogTemplate());
+                writer.Close();
+            }
+        }
+
+        private string GetLogTemplate()
+        {
+            var template = new StringBuilder();
+
+            template.Append(DateTime.Now);
+            template.Append(", ");
+            template.Append(System.Environment.MachineName);
+            template.Append(", ");
+            template.Append(System.Environment.UserName);
+
+            return template.ToString();
+        }
+    }
+}
